70d63c83f619383c7f38d5063a953e9997bb31cd
benoit sommariva <benoitsommariva@gmail.com>
2022-02-22 11:27:13 +0100

    model vehicle route + views index et show

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,46 @@
+class VehiclesController < ApplicationController
+
+  before_action :find_vehicle, only: [:show, :edit, :update]
+
+
+  def index
+    @vehicles = Vehicle.all
+  end
+
+  def show
+  end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    if @vehicle.save
+      redirect_to vehicle_path(@vehicle)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @vehicle.update(vehicle_params)
+    redirect_to vehicle_path(@vehicle)
+  end
+
+
+
+  private
+
+  def find_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def vehicle_params
+    params.require(:vehicle).permit(:name, :description, :price)
+  end
+
+end
