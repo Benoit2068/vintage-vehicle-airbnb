1df5367c8ced3084fb3f47431faf746af39b525f
Boubacar Kipre <alanvalenciakipre@gmail.com>
2022-03-01 21:03:10 +0100

    search bar for vehicules

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,7 @@
 class Vehicle < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
+
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 10 }
   validates :price, presence: true
