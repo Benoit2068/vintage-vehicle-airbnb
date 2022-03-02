9aa806ea8bc12c33c0f355b51281e027751653cc
benoit sommariva <benoitsommariva@gmail.com>
2022-03-02 22:15:25 +0100

    show reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,11 +11,22 @@ class ReservationsController < ApplicationController
     @vehicle = Vehicle.find(params[:vehicle_id])
     @reservation.vehicle = @vehicle
     @reservation.user = current_user
-    if @reservation.save
-      redirect_to vehicle_path(@reservation.vehicle)
+    if @reservation.date_start < @reservation.date_end
+      if @reservation.save
+        # redirect_to vehicle_path(@reservation.vehicle)
+        redirect_to reservation_path(@reservation)
+      else
+        render :new
+      end
     else
       render :new
     end
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+    @duration = (@reservation.date_end - @reservation.date_start).to_i
+    @reservation_price = @duration * @reservation.vehicle.price.to_i
   end
 
 
